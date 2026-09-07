4a701f4ac2240f5b4eb4160256d115e767e48228
extencil <extencil@segfault.net>
2026-09-06 23:32:28 -0300

    smtp submission stuff

--- __db/smtp_sender_acl.sql
+++ __db/smtp_sender_acl.sql
@@ -4,5 +4,6 @@ CREATE TABLE `smtp_sender_acl` (
   `sender` varchar(320) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uniq_login_sender` (`login`,`sender`)
+  UNIQUE KEY `uniq_login_sender` (`login`,`sender`),
+  KEY `idx_sender_active` (`sender`, `active`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
